0fe4b4a0c8459400e1b20d17e6c76eafbdd419fc
nitishatcodexxa <138878506+nitishatcodexxa@users.noreply.github.com>
2023-10-17 03:10:59 +0530

    Update config.js

--- connection/config.js
+++ connection/config.js
@@ -3,8 +3,8 @@ module.exports={
    // url:"mongodb://127.0.0.1:27017/Boroplast"
 
     //url:"mongodb+srv://913954956no:913954956@cluster0.fn3r32o.mongodb.net/mgmt?retryWrites=true&w=majority"
-      url:"mongodb+srv://nitish:9139549561nitish@api.jqnwwdt.mongodb.net/boroplast?retryWrites=true&w=majority"
-     //url:"mongodb+srv://nitish:9139549561nitish@cluster0.fn3r32o.mongodb.net/boroplast?retryWrites=true&w=majority"
+      url:"mongodb+srv://nitishss:9139549561nitish@api.jqnwwdt.mongodb.net/boroplast?retryWrites=true&w=majority"
+     //url:"mongodb+srv://nitishss:9139549561nitish@cluster0.fn3r32o.mongodb.net/boroplast?retryWrites=true&w=majority"
 }
 
 
